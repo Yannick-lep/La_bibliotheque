ef7ed66f7c3fff2b7e875262089a20a31b007792
Benjamin <Benjamin.huynh.marais@gmail.com>
2026-01-13 14:30:47 +0100

    ajout de routes pour la gestion des livres et des utilisateurs

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,13 @@
 
 $routes = [
     'home' => 'home.php',
-    'login' => 'login/login.php'
+    'login' => 'login/login.php',
+    'add-livres' => 'livres/add-livres.php',
+    'del-livres' => 'livres/del-livres.php',
+    'edit-livres' => 'livres/edit-livres.php',
+    'list-livres' => 'livres/list-livres.php',
+    'add-utilisateur' => 'utilisateur/add-utilisateur.php',
+    'del-utilisateur' => 'utilisateur/del-utilisateur.php',
+    'edit-utilisateur' => 'utilisateur/edit-utilisateur.php',
+    'list-utilisateur' => 'utilisateur/list-utilisateur.php'
 ];
