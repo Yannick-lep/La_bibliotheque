de926e05825857f219dd39f76db4498548751386
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-01-13 16:06:54 +0100

    Ajout page 404

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php
 require PHP_ROOT . "/views/partials/header.php"; ?>
 
-<h1 style="text-align: center;">Erreur 404 !</h1>
-<p>Le document que vous avez demandé est introuvable !</p>
+<h1 style="text-align: center; color: lightcoral; margin: 2rem 0; font-size: 2rem;">Erreur 404 !</h1>
+<p style="text-align: center; color: antiquewhite; font-size: 1.2rem;">Le document que vous avez demandé est introuvable !</p>
 
 <?php require PHP_ROOT . "/views/partials/footer.php"; ?>
