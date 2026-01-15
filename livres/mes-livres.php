b52c6dfaca52a055f4882d2f217728e980389136
Yannick-lep <lepinayyannick7@gmail.com>
2026-01-15 10:43:29 +0100

    ajout historique emprunt abonne

--- livres/mes-livres.php
+++ livres/mes-livres.php
@@ -4,6 +4,12 @@ require PHP_ROOT . '/includes/functions-emprunts.php';
 
 $id_utilisateur = $_SESSION['id_utilisateur'];
 
+// Emprunt en cours
 $livres = getEmpruntsUtilisateur($pdo, $id_utilisateur);
 
+// Historique
+$historique = [];
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['envoyer'])){
+    $historique = getHistoriqueEmpruntsUtilisateur($pdo, $id_utilisateur, 5);
+}
 require PHP_ROOT . '/views/livres/mes-livres-view.php';
