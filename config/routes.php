89e343e30e95bc0b0019e52bd0e1189aa5583600
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-01-15 12:14:04 +0100

    Mise a jour du routeur pour gerer les droits

--- config/routes.php
+++ config/routes.php
@@ -6,24 +6,32 @@ $routes = [
     '500' => '500.php',
     'login' => 'login/login.php',
     'register' => 'login/register.php',
+    'search-livres' => 'livres/search-livres.php'
+];
+
+$routes_abonne = [
+    'profil-utilisateur' => 'utilisateur/profil-utilisateur.php',
+    'mes-livres' => 'livres/mes-livres.php'
+];
+
+$routes_admin = [
     'add-livres' => 'livres/add-livres.php',
     'del-livres' => 'livres/del-livres.php',
     'edit-livres' => 'livres/edit-livres.php',
     'list-livres' => 'livres/list-livres.php',
-    'search-livres' => 'livres/search-livres.php',
-    'resultats-livres' => 'livres/resultats-livres.php',
     'add-utilisateur' => 'utilisateur/add-utilisateur.php',
     'del-utilisateur' => 'utilisateur/del-utilisateur.php',
     'edit-utilisateur' => 'utilisateur/edit-utilisateur.php',
     'list-utilisateur' => 'utilisateur/list-utilisateur.php',
-    'profil-utilisateur' => 'utilisateur/profil-utilisateur.php',
     'add-emprunts' => 'emprunts/add-emprunts.php',
     'del-emprunts' => 'emprunts/del-emprunts.php',
     'edit-emprunts' => 'emprunts/edit-emprunts.php',
     'list-emprunts' => 'emprunts/list-emprunts.php',
+];
+
+$routes_employe = [
     'depart-emprunts' => 'emprunts/depart-emprunts.php',
     'retour-emprunts' => 'emprunts/retour-emprunts.php',
-    'mes-livres' => 'livres/mes-livres.php',
     'validate-retour-emprunts' => 'emprunts/validate-retour-emprunts.php',
     'validate-depart-emprunts' => 'emprunts/validate-depart-emprunts.php'
-    ];
+];
--- index.php
+++ index.php
@@ -4,15 +4,42 @@ session_start();
 require 'includes/dbconnexion.php';
 require 'config/routes.php';
 
-$pageFiltre = filter_input(INPUT_GET, 'page' , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$pageFiltre = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $page = $pageFiltre ?? 'home';
 
-if (!array_key_exists($page,$routes)) {
+if (is_admin()) {
+    if (array_key_exists($page, $routes_admin)) {
+        if (file_exists($routes_admin[$page])) {
+            require $routes_admin[$page];
+            die();
+        }
+    }
+} elseif (is_employe()) {
+    if (array_key_exists($page, $routes_employe)) {
+        if (file_exists($routes_employe[$page])) {
+            require $routes_employe[$page];
+            die();
+        }
+    }
+} elseif (is_abonne()) {
+    if (array_key_exists($page, $routes_abonne)) {
+        if (file_exists($routes_abonne[$page])) {
+            require $routes_abonne[$page];
+            die();
+        }
+    }
+}
+
+/*
+ * Generic routing table
+ */
+
+if (!array_key_exists($page, $routes)) {
     $page = '404';
 }
 
 if (!file_exists($routes[$page])) {
-    $page = '404';
+    $page = '500';
 }
 
 require $routes[$page];
